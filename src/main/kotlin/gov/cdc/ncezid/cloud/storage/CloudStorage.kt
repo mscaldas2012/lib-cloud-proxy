9b17358624339558433bff45fc5d663e5a74da5e
Glenn Phillips <qkw9@cdc.gov>
2021-03-17 20:44:36 -0400

    Updated meta content values to not include the 'meta' prefix

--- src/main/kotlin/gov/cdc/ncezid/cloud/storage/CloudStorage.kt
+++ src/main/kotlin/gov/cdc/ncezid/cloud/storage/CloudStorage.kt
@@ -7,9 +7,9 @@ import java.io.InputStream
 // TODO - consider splitting interface, one with default bucket and one without
 
 
-const val META_CONTENT_LENGTH = "meta-content-length"
-const val META_CONTENT_TYPE = "meta-content-type"
-const val META_CONTENT_ENCODING = "meta-content-encoding"
+const val META_CONTENT_LENGTH = "content-length"
+const val META_CONTENT_TYPE = "content-type"
+const val META_CONTENT_ENCODING = "content-encoding"
 
 /**
  *
